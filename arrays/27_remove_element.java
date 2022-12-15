3bd3e90b6457c7465a5c4dc0b7525073a3541789
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-12-15 17:18:50 +0300

    Create 27_remove_element.java

---
+++ arrays/27_remove_element.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        for(int i = 0; i<n; i++)
+            if(nums[i] == val)
+                nums[i--] = nums[n-- -1];
+        return n;
+    }
+}
